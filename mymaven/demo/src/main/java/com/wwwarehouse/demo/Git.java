253b7f6f350d93c4a0ad813cfa170448fa821b2e
chaoyong.qin <chaoyong.qin@wwwarehouse.com>
2017-05-10 14:31:44 +0800

    添加Git.java文件

---
+++ mymaven/demo/src/main/java/com/wwwarehouse/demo/Git.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.wwwarehouse.demo;
+
+/**
+ * @author chaoyong.qin
+ *
+ */
+public class Git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (String string : args) {
+			System.out.println(string);
+		}
+
+	}
+
+}

f54883b0c6966c4701937ef414b95e25b0e2fbeb
chaoyong.qin <chaoyong.qin@wwwarehouse.com>
2017-05-10 11:33:48 +0800

    修改jedis的获取方式

--- mymaven/demo/src/main/java/com/wwwarehouse/demo/RedisJava.java
+++ mymaven/demo/src/main/java/com/wwwarehouse/demo/RedisJava.java
@@ -9,6 +9,7 @@ import redis.clients.jedis.Jedis;
 public class RedisJava {
 	public static void main(String[] args) {
 		// 连接本地的 Redis 服务
+//		Jedis jedis = new Jedis("localhost",6379,5000);
 		Jedis jedis = new Jedis("localhost");
 		System.out.println("Connection to server sucessfully");
 

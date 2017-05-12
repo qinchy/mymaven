9f9014b1d837b4442fba13d78a8358e11b75561c
chaoyong.qin <chaoyong.qin@wwwarehouse.com>
2017-05-12 16:52:19 +0800

    添加design-pattern工程

--- design-pattern/src/main/java/com/design/my/abstractfactory/Main.java
+++ design-pattern/src/main/java/com/design/my/abstractfactory/Main.java
@@ -4,6 +4,7 @@
 package com.design.my.abstractfactory;
 
 /**
+ * 抽象工厂模式（Abstract Factory Pattern）是围绕一个超级工厂创建其他工厂。该超级工厂又称为其他工厂的工厂。
  * @author chaoyong.qin
  *
  */
@@ -15,12 +16,6 @@ public class Main {
 	public static void main(String[] args) {
 		// 获取形状工厂
 		AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE");
-
-		// 获取形状为 Circle 的对象
-		Shape shape1 = shapeFactory.getShape("CIRCLE");
-
-		// 调用 Circle 的 draw 方法
-		shape1.draw();
 
 		// 获取形状为 Rectangle 的对象
 		Shape shape2 = shapeFactory.getShape("RECTANGLE");
@@ -34,6 +29,8 @@ public class Main {
 		// 调用 Square 的 draw 方法
 		shape3.draw();
 
+		System.out.println("========================================");
+		
 		// 获取颜色工厂
 		AbstractFactory colorFactory = FactoryProducer.getFactory("COLOR");
 
@@ -48,12 +45,6 @@ public class Main {
 
 		// 调用 Green 的 fill 方法
 		color2.fill();
-
-		// 获取颜色为 Blue 的对象
-		Color color3 = colorFactory.getColor("BLUE");
-
-		// 调用 Blue 的 fill 方法
-		color3.fill();
 
 	}
 
